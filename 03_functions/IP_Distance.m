ab757b1a99f9927adda4375155d994af76f11633
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-03-07 14:27:57 +0000

    Created IP Distance Function

--- 03_functions/IP_Distance.m
+++ 03_functions/IP_Distance.m
@@ -1,16 +1,49 @@
-function [outputArg1,outputArg2] = IP_Distance(inputArg1,inputArg2)
-%IP_DISTANCE Summary of this function goes here
-%   Detailed explanation goes here
+function [xN_IP] = IP_Distance(xN, Ax, Norm_Ax, K);
+%IP_DISTANCE Select K initial points based on euclidean distance.
+%   Inputs: xN, Ax, Norm_Ax, Ax, K
+%   xN may contain just LE and TE to begin with. 
+%   K must be smaller than Ax
 arguments (Input)
-    inputArg1
-    inputArg2
+    xN                   % Current List of Initial Points
+    Ax                   % Complete List of Aerofoil Points
+    Norm_Ax              % Norms Between All Aerofoil Points
+    K                    % Number of Initial Points to Select
 end
 
 arguments (Output)
-    outputArg1
-    outputArg2
+    xN_IP                % New List of Initial Points
 end
 
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+%% FUNCTION BODY
+num_existing = size(xN, 1);
+
+% 1. Map existing xN coordinates to their row indices in Ax
+% Preallocate the array for existing points + K new points
+selected_idx = zeros(num_existing + K, 1);
+for i = 1:num_existing
+    [~, idx] = min(sum((Ax - xN(i, :)).^2, 2));
+    selected_idx(i) = idx;
+end
+
+% 2. Initialize the running minimum distance array
+% Extract columns for all currently selected points and find the minimum row-wise
+min_dist = min(Norm_Ax(:, selected_idx(1:num_existing)), [], 2);
+
+% 3. Iterative Selection Loop
+for i = 1:K
+    % Find the point that maximizes the minimum distance
+    [~, next_idx] = max(min_dist);
+    
+    % Store the new index
+    current_step = num_existing + i;
+    selected_idx(current_step) = next_idx;
+    
+    % Update the running minimum distance directly from the Norm_Ax matrix
+    min_dist = min(min_dist, Norm_Ax(:, next_idx));
+end
+
+% 4. Extract final coordinates
+xN_IP = Ax(selected_idx, :);
+
+
 end
