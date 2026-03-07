f29e6f0fb16a2a8583e9440a8a1bff1f23f25e31
Adam Driver <adam.driver.2022@bristol.ac.uk>
2026-03-07 17:25:22 +0000

    Update Greedy Inputs

--- 03_functions/Greedy.m
+++ 03_functions/Greedy.m
@@ -1,16 +1,18 @@
-function [outputArg1,outputArg2] = Greedy(xN,Act_DEx, )
-%GREEDY Do K iterations of Greedy Deformation
-%   Detailed explanation goes here
+function [xN_idx_out, max_err_history] = Greedy(xN_idx, DAx_act, Norm_Ax, Phi, SF_R, options)
+% GREEDY_SELECTION Adds control points based on maximum deformation error.
+
 arguments (Input)
-    inputArg1
-    inputArg2
+    xN_idx               % Initial Point Indices
+    DAx_act              % True Deformation at all nodes
+    Norm_Ax              % Precomputed Distance Matrix
+    Phi                  % Basis Function handle
+    SF_R                 % Support Radius
+    
+    % Optional Name-Value pairs with default fail-safes
+    options.err_tol (1,1) double = 0     % Defaults to 0 so it never triggers if uncalled
+    options.K       (1,1) double = Inf   % Defaults to Inf (max iterations) if uncalled
 end
-
 arguments (Output)
-    outputArg1
-    outputArg2
-end
-
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
+    xN_idx_out            % Final list of Control Point Indices
+    max_err_history      % Array tracking the maximum error at each step
 end
